6d1dca664941d50bb5f1d9479452b8d07656d1eb
meh jav <mehhran@gmail.com>
2021-12-09 19:59:10 +0330

    models

--- tagsApp/models.py
+++ tagsApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class ReadTagsOrder(models.Model):
+    
+    scan_time = models.DateTimeField()
+
+    def __str__ (self):
+        return "Read at: " + str(self.scan_time)
+    
+
+class Tag(models.Model):
+    
+    strEPC = models.CharField(max_length=100)
+    antenna = models.IntegerField()
+    strRSSI = models.CharField(max_length=100)
+    nReadCount = models.IntegerField()
+    
+    readtagorder = models.ForeignKey(ReadTagsOrder, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.strEPC + " - " + str(self.readtagorder)
