95c875a0b3abea6c3fc6fef03a6cbe70222183aa
meh jav <mehhran@gmail.com>
2021-12-09 19:58:48 +0330

    added the app

--- tagsProject/settings.py
+++ tagsProject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tagsApp',
 ]
 
 MIDDLEWARE = [
