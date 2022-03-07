49282cf729f64893caa8fabf3b88505c1d38f000
Mehran J <mehhran@gmail.com>
2022-03-07 15:54:02 -0800

    debug=True, added rest_framework to apps

--- tagsProject/settings.py
+++ tagsProject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tagsApp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
